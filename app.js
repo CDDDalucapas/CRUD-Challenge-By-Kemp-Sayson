aa01d5939d5c38cf211e107a12554bcc4d42f682
Christian Dave Dalucapas <christiandave.dalucapas@gmail.com>
2021-04-01 19:16:00 +0800

    Added Additional routes and cookieParser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require('path');
 const mysql = require("mysql");
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 
 dotenv.config({
     path: './.env'
@@ -19,6 +20,11 @@ const db = mysql.createConnection({
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
 
+//Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: false}));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.use(cookieParser());
 
 app.set('view engine', 'hbs');
 
@@ -30,10 +36,11 @@ db.connect((error) => {
     }
 })
 
-app.get("/", (req, res) => {
-    // res.send("<h1>Home Page</h1>")
-    res.render("index")
-})
+//Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
+
 
 app.listen(3000, () =>{
     console.log("Server started on Port 5000");
